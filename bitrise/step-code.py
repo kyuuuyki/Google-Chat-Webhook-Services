02f1ab6046fa42ad58294c85cc16866965d05a23
Chayanon Ardkham <chayanon.ard@ascendcorp.com>
2021-01-04 11:27:00 +0700

    Version 0.6.0
    - Introducing CardView!

--- bitrise/step-code.py
+++ bitrise/step-code.py
@@ -7,89 +7,55 @@ from httplib2 import Http
 def isStringBlank(string):
     return not (string and string.strip())
 
-def main():
+def getThreadKey(applicationName, mergeRequestNumber):
+    threadKey = ""
 
-    GOOGLE_CHAT_WEBHOOK_URL = str(sys.argv[1])
+    """Merge Request"""
+    if not isStringBlank(mergeRequestNumber):
+        threadKey = re.sub('[^A-Za-z0-9]+', '', applicationName) + "-merge-request-" + mergeRequestNumber
 
-    JIRA_ORGANIZATION_NAME = str(sys.argv[2])
-    JIRA_TEAM_NAME = str(sys.argv[3])
+    return threadKey
 
-    BITRISE_APP_TITLE = str(sys.argv[4])
-    BITRISE_BUILD_STATUS = str(sys.argv[5])
-    BITRISE_BUILD_NUMBER = str(sys.argv[6])
-    BITRISE_TRIGGERED_WORKFLOW_TITLE = str(sys.argv[7])
-    BITRISE_BUILD_URL = str(sys.argv[8])
-    BITRISE_PUBLIC_INSTALL_PAGE_URL = str(sys.argv[9])
+def getConfigurations(configurationString):
+    CONFIGURATIONS = []
+    if not isStringBlank(configurationString):
+        CONFIGURATIONS = re.findall(r"[\w']+", configurationString)
+        print(CONFIGURATIONS)
+    return CONFIGURATIONS
 
-    BITRISE_GIT_BRANCH = str(sys.argv[10])
-    BITRISE_GIT_MESSAGE = str(sys.argv[11])
-    
-    BITRISE_PULL_REQUEST = str(sys.argv[12])
-    BITRISEIO_GIT_BRANCH_DEST = str(sys.argv[13])
-    BITRISEIO_PULL_REQUEST_REPOSITORY_URL = str(sys.argv[14])
-    
-    TEAM_DEVELOPER_USERIDS = str(sys.argv[15])
-    TEAM_TESTER_USERIDS = str(sys.argv[16])
-
-    BUILD_NOTE = str(sys.argv[17])
-
-    """INTERNAL VARIABLES"""
-    THREAD_KEY = ""
-
-    """STATUS"""
-    if BITRISE_BUILD_STATUS == "0":
-        BUILD_STATUS_TEXT = "Build Succeeded!"
+def getBuildStatusTitle(buildStatus):
+    if buildStatus == "0":
+        return "Build Succeeded!"
     else:
-        BUILD_STATUS_TEXT = "Build Failed"
-        
-    """MERGE STATUS"""
-    if not isStringBlank(BITRISE_PULL_REQUEST):
+        return "Build Failed"
 
-        PULL_REQUEST_URL = BITRISEIO_PULL_REQUEST_REPOSITORY_URL.split("@")
-        PULL_REQUEST_URL = PULL_REQUEST_URL[1]
-        PULL_REQUEST_URL = PULL_REQUEST_URL.replace(":", "/")
-        PULL_REQUEST_URL = PULL_REQUEST_URL.replace(".git", "")
+def getBuildStatusIcon(buildStatus):
+    if buildStatus == "0":
+        return "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/144/apple/271/party-popper_1f389.png"
+    else:
+        return "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/144/apple/271/fire_1f525.png"
 
-        if "bitbucket" in PULL_REQUEST_URL:
-            PULL_REQUEST_URL = PULL_REQUEST_URL + "/pull-requests/" + BITRISE_PULL_REQUEST
+def getMergeRequestURL(mergeRequestNumber, repositoryURL):
+    PULL_REQUEST_URL = repositoryURL.split("@")
+    PULL_REQUEST_URL = PULL_REQUEST_URL[1]
+    PULL_REQUEST_URL = PULL_REQUEST_URL.replace(":", "/")
+    PULL_REQUEST_URL = PULL_REQUEST_URL.replace(".git", "")
 
-        elif "gitlab" in PULL_REQUEST_URL:
-            PULL_REQUEST_URL = PULL_REQUEST_URL + "/-/merge_requests/" + BITRISE_PULL_REQUEST
+    if "bitbucket" in PULL_REQUEST_URL:
+        PULL_REQUEST_URL = PULL_REQUEST_URL + "/pull-requests/" + mergeRequestNumber
+    elif "gitlab" in PULL_REQUEST_URL:
+        PULL_REQUEST_URL = PULL_REQUEST_URL + "/-/merge_requests/" + mergeRequestNumber
 
-        BUILD_STATUS_TEXT = "Merge Request <https://" + PULL_REQUEST_URL + "|#" + BITRISE_PULL_REQUEST + "> " + BUILD_STATUS_TEXT
-        THREAD_KEY = re.sub('[^A-Za-z0-9]+', '', BITRISE_APP_TITLE) + "-MERGE_REQUEST-" + BITRISE_PULL_REQUEST
-            
-    CHAT_TEXT = "*" + BITRISE_APP_TITLE + " | " + BUILD_STATUS_TEXT + "*"
+    return PULL_REQUEST_URL
 
-    """BUILD NUMBER"""
-    CHAT_TEXT = CHAT_TEXT + "\n<" + BITRISE_BUILD_URL + "|#" + BITRISE_BUILD_NUMBER + ">"
-
-    """PUBLIC INSTALL LINK"""
-    if not isStringBlank(BITRISE_PUBLIC_INSTALL_PAGE_URL):
-        CHAT_TEXT = CHAT_TEXT + " • <" + BITRISE_PUBLIC_INSTALL_PAGE_URL + "|Open public install page>"
-
-    CHAT_TEXT = CHAT_TEXT + "\n"
-
-    """BUILD NOTE"""
-    if not isStringBlank(BUILD_NOTE):
-        CHAT_TEXT = CHAT_TEXT + "\n" + BUILD_NOTE + "\n"
-    
-    """BRANCH DATA"""
-    BRANCH_DATA_TEXT = "\nBranch: `" + BITRISE_GIT_BRANCH + "`"
-    if not isStringBlank(BITRISE_PULL_REQUEST):
-        BRANCH_DATA_TEXT = BRANCH_DATA_TEXT + " → `" + BITRISEIO_GIT_BRANCH_DEST + "`"
-
-    """WORKFLOW"""
-    CHAT_TEXT = CHAT_TEXT + BRANCH_DATA_TEXT + "\nWorkflow: `" + BITRISE_TRIGGERED_WORKFLOW_TITLE + "`"
-
-    """JIRA LINK"""
-    if not isStringBlank(BITRISE_GIT_MESSAGE) and not isStringBlank(JIRA_TEAM_NAME):
-
+def getJIRAContent(gitMessage, jiraOrganizationName, jiraTeamName):
+    jiraContent = ""
+    if not isStringBlank(gitMessage) and not isStringBlank(jiraTeamName):
         print("\nExtracted Jira Issue Number (s):")
     
         JIRA_ISSUE_NUMBERS = []
         
-        JIRA_ISSUE_NUMBERS_FRAGMENTS = BITRISE_GIT_MESSAGE.split(JIRA_TEAM_NAME)
+        JIRA_ISSUE_NUMBERS_FRAGMENTS = gitMessage.split(jiraTeamName)
         JIRA_ISSUE_NUMBERS_FRAGMENTS = JIRA_ISSUE_NUMBERS_FRAGMENTS[1:]
 
         for JIRA_ISSUE_NUMBERS_FRAGMENT in JIRA_ISSUE_NUMBERS_FRAGMENTS:
@@ -107,59 +73,235 @@ def main():
 
         if len(JIRA_ISSUE_NUMBERS) > 0:
 
-            CHAT_TEXT = CHAT_TEXT + "\nJira Task(s): "
-
             for JIRA_ISSUE_NUMBER in JIRA_ISSUE_NUMBERS:
-                JIRA_URL = "https://" + JIRA_ORGANIZATION_NAME + ".atlassian.net/browse/" + JIRA_TEAM_NAME + "-" + JIRA_ISSUE_NUMBER
-                CHAT_TEXT = CHAT_TEXT + "<" + JIRA_URL + "|" + JIRA_TEAM_NAME + "-" + JIRA_ISSUE_NUMBER + ">, "
+                JIRA_URL = "https://" + jiraOrganizationName + ".atlassian.net/browse/" + jiraTeamName + "-" + JIRA_ISSUE_NUMBER
+                jiraContent = jiraContent + "<a href=" + JIRA_URL + ">" + jiraTeamName + "-" + JIRA_ISSUE_NUMBER + "</a><br>"
 
-            CHAT_TEXT = CHAT_TEXT[:-2]
-        
-    """GIT MESSAGE"""
-    if not isStringBlank(BITRISE_GIT_MESSAGE):
-        CHAT_TEXT = CHAT_TEXT + "\n```" + BITRISE_GIT_MESSAGE + "```"
-        
-    """NOTIFY DEVELOPERS & TESTERS"""
+    return jiraContent
+
+def getTaggedUsersContent(developerUserIDs, testerUserIDs, mergeRequestNumber, buildInstallURL):
+    usersContent = ""
     USER_IDS = []
     
-    if (not isStringBlank(TEAM_DEVELOPER_USERIDS)) and (not isStringBlank(BITRISE_PULL_REQUEST)):
+    if (not isStringBlank(developerUserIDs)) and (not isStringBlank(mergeRequestNumber)):
         
         print("\nExtracted Developer UserID (s):")
         
-        TEAM_DEVELOPER_IDS = re.findall(r"[\w']+", TEAM_DEVELOPER_USERIDS)
+        TEAM_DEVELOPER_IDS = re.findall(r"[\w']+", developerUserIDs)
         for TEAM_DEVELOPER_ID in TEAM_DEVELOPER_IDS:
         
             print(TEAM_DEVELOPER_ID)
             USER_IDS.append(TEAM_DEVELOPER_ID)
     
-    if (not isStringBlank(TEAM_TESTER_USERIDS)) and (not isStringBlank(BITRISE_PUBLIC_INSTALL_PAGE_URL)):
+    if (not isStringBlank(testerUserIDs)) and (not isStringBlank(buildInstallURL)):
         
         print("\nExtracted Tester UserID (s):")
         
-        TEAM_TESTER_IDS = re.findall(r"[\w']+", TEAM_TESTER_USERIDS)
+        TEAM_TESTER_IDS = re.findall(r"[\w']+", testerUserIDs)
         for TEAM_TESTER_ID in TEAM_TESTER_IDS:
         
             print(TEAM_TESTER_ID)
             USER_IDS.append(TEAM_TESTER_ID)
             
     if len(USER_IDS) > 0:
-
-        CHAT_TEXT = CHAT_TEXT + "\n"
-
         for USER_ID in USER_IDS:
-            CHAT_TEXT = CHAT_TEXT + "<users/" + USER_ID + "> "
+            usersContent = usersContent + "<users/" + USER_ID + "> "
+
+    return usersContent
+
+def main():
+
+    scriptURL = str(sys.argv[1])
+
+    jiraOrganizationName = str(sys.argv[2])
+    jiraTeamName = str(sys.argv[3])
+
+    applicationName = str(sys.argv[4])
+
+    buildStatus = str(sys.argv[5])
+    buildNumber = str(sys.argv[6])
+    buildWorkflowName = str(sys.argv[7])
+    buildURL = str(sys.argv[8])
+    buildInstallURL = str(sys.argv[9])
+
+    gitBranchName = str(sys.argv[10])
+    gitMessage = str(sys.argv[11])
+    
+    mergeRequestNumber = str(sys.argv[12])
+    mergeRequestDestinationBranchName = str(sys.argv[13])
+
+    repositoryURL = str(sys.argv[14])
+    
+    developerUserIDs = str(sys.argv[15])
+    testerUserIDs = str(sys.argv[16])
+
+    buildNote = str(sys.argv[17])
+
+    configurationString = str(sys.argv[18])
+
+    """INTERNAL VARIABLES"""
+    threadKey = getThreadKey(applicationName, mergeRequestNumber)
+    configurationOptions = getConfigurations(configurationString)
+
+    """Card Header"""
+    cardHeaderTitle = "<b>" + getBuildStatusTitle(buildStatus) + "</b>"
+    cardHeaderSubtitle = applicationName + " | " + gitBranchName
+    cardHeaderImageUrl = getBuildStatusIcon(buildStatus)
+    cardHeader = {
+        "title": cardHeaderTitle, 
+        "subtitle": cardHeaderSubtitle,
+        "imageUrl": cardHeaderImageUrl,
+        "imageStyle": "IMAGE"
+    }
+    
+    cardContents = []
+    
+    """Merge Request Section"""
+    mergeRequestURL = getMergeRequestURL(mergeRequestNumber, repositoryURL)
+    if not isStringBlank(mergeRequestNumber):
+        mergeRequestSection = {
+            "widgets": [
+                {
+                    "keyValue": {
+                        "topLabel": "Request to merge into",
+                        "content": mergeRequestDestinationBranchName,
+                        "contentMultiline": "false",
+                        "onClick": {
+                            "openLink": {
+                                "url": mergeRequestURL
+                            }
+                        },
+                        "iconUrl": "https://www.seekicon.com/free-icon-download/git-pull-request-outline-icon_1.png",
+                        "button": {
+                            "textButton": {
+                                "text": "#" + mergeRequestNumber,
+                                "onClick": {
+                                    "openLink": {
+                                        "url": mergeRequestURL
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            ]
+        }
+        cardContents.append(mergeRequestSection)
+
+    """Detail Section"""
+    if not ("isBuildDetailDisabled" in configurationOptions):
+        detailSectionWidgets = [
+            {
+                "keyValue": {
+                    "topLabel": "Build No.",
+                    "content": "#" + buildNumber,
+                    "onClick": {
+                        "openLink": {
+                            "url": buildURL
+                        }
+                    }
+                }
+            }
+        ]
+
+        if not ("isBuildWorkflowDisabled" in configurationOptions):
+            workflowWidget = {
+                "keyValue": {
+                    "topLabel": "Workflow",
+                    "content": buildWorkflowName
+                }
+            }
+            detailSectionWidgets.append(workflowWidget)
+
+        if not isStringBlank(buildNote):
+            buildNoteWidget = {
+                "keyValue": {
+                    "topLabel": "Note",
+                    "content": buildNote
+                }
+            }
+            detailSectionWidgets.append(buildNoteWidget)
+
+        detailSection = {
+            "header": "Details",
+            "widgets": detailSectionWidgets
+        }
+        cardContents.append(detailSection)
+
+    """Jira Section"""
+    jiraContent = getJIRAContent(gitMessage, jiraOrganizationName, jiraTeamName)
+    if not isStringBlank(jiraContent):
+        jiraContentSection = {
+            "header": "Related Issue(s)",
+            "widgets": [
+                {
+                    "textParagraph": {
+                        "text": jiraContent
+                    }
+                }
+            ]
+        }
+        cardContents.append(jiraContentSection)
+
+    """Git Message Section"""
+    if (not isStringBlank(gitMessage)) and ("isGitMessageEnabled" in configurationOptions):
+        gitMessageSection = {
+            "header": "Git Message",
+            "widgets": [
+                {
+                    "textParagraph": {
+                        "text": gitMessage
+                    }
+                }
+            ]
+        }
+        cardContents.append(gitMessageSection)
+
+    """Public Install Page"""
+    if not isStringBlank(buildInstallURL):
+        buildInstallSection = {
+            "widgets": [
+                {
+                    "buttons": [
+                        {
+                            "textButton": {
+                                "text": "Public Install Page",
+                                "onClick": {
+                                    "openLink": {
+                                        "url": buildInstallURL
+                                    }
+                                }
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
+        cardContents.append(buildInstallSection)
+
+    """Card Data"""
+    card = [
+        {
+            "header": cardHeader,
+            "sections": cardContents
+        }
+    ]
+
+    """Notify Developers"""
+    text = getTaggedUsersContent(developerUserIDs, testerUserIDs, mergeRequestNumber, buildInstallURL)
         
     """POST MESSAGE"""
     bot_message = {
-        'text' : CHAT_TEXT
+        'text': text,
+        'cards': card
     }
     message_headers = {'Content-Type': 'application/json; charset=UTF-8'}
 
     http_obj = Http()
 
-    URL = GOOGLE_CHAT_WEBHOOK_URL
-    if (not isStringBlank(TEAM_DEVELOPER_USERIDS)):
-        URL = URL + "&threadKey=" + THREAD_KEY
+    URL = scriptURL
+    if not isStringBlank(threadKey):
+        URL = URL + "&threadKey=" + threadKey
 
     response = http_obj.request(
         uri=URL,
